2b0ae236c87b1bb6fdaae990b27aa2b261be9613
Shaaim <shaaimgazi10@gmail.com>
2024-09-09 11:29:10 +0530

    dt: adapt IX

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,16 +2,16 @@
 # Copyright (C) 2021 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
-#
+# 
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_Mi439.mk \
-    $(LOCAL_DIR)/lineage_Mi439_4_19.mk
+    $(LOCAL_DIR)/infinity_Mi439.mk \
+    $(LOCAL_DIR)/infinity_Mi439_4_19.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_Mi439-user \
-    lineage_Mi439-userdebug \
-    lineage_Mi439-eng \
-    lineage_Mi439_4_19-user \
-    lineage_Mi439_4_19-userdebug \
-    lineage_Mi439_4_19-eng
+    infinity_Mi439-user \
+    infinity_Mi439-userdebug \
+    infinity_Mi439-eng \
+    infinity_Mi439_4_19-user \
+    infinity_Mi439_4_19-userdebug \
+    infinity_Mi439_4_19-eng
